d7b80101ad473e2054b27fa60c7541237f5f8d26
amasood <amasood@octans.ai>
2022-03-28 22:16:39 +0500

    Created the UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { FilterComponent } from './shopping-cart/filter/filter.component';
+import { ProductListComponent } from './shopping-cart/product-list/product-list.component';
+import { CartComponent } from './shopping-cart/cart/cart.component';
+import { CartItemComponent } from './shopping-cart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './shopping-cart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingCartComponent,
+    FilterComponent,
+    ProductListComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule
